18fc130d595b634a4b6dc0c90f993aa8f6d45804
Lwandle Dlamini <babekizulu@gmail.com>
2025-03-21 13:07:31 +0200

    We are going to the moon!

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@libs': path.resolve(__dirname, './src/libs'),
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -13,4 +21,15 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          data: ['@libs/alexandriiia/mechanicalInventions.js']
+        }
+      }
+    }
+  }
 })
